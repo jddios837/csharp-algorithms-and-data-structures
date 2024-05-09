8f286c0ceae011963a6e773b028a0929c6cff1e4
jddios837 <jddios837@gmail.com>
2024-05-08 19:46:33 -0500

    adding function addUpToFirst & addUpToSecond

--- 1-BigONotation-01/Program.cs
+++ 1-BigONotation-01/Program.cs
@@ -9,4 +9,23 @@ for (int i = normalString.Length - 1; i >= 0; i--)
     reversedString += normalString[i];
 }
 
-Console.Write(reversedString);
+Console.WriteLine(reversedString);
+Console.WriteLine(addUpToFirst(100));
+Console.WriteLine(addUpToSecond(100));
+
+int addUpToFirst(int n)
+{
+    int total = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        total += i;
+    }
+    
+    return total;
+}
+
+int addUpToSecond(int n)
+{
+    return n * (n + 1) / 2;
+}
