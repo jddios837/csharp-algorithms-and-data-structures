9a8b4f97085615dbfd1bf54acff162508eaffdea
jddios837 <jddios837@gmail.com>
2024-05-11 12:38:52 -0500

    Initial version CharStringCount function

--- 2-problem-solving-approach/Program.cs
+++ 2-problem-solving-approach/Program.cs
@@ -25,10 +25,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("String Counter");
+
+        var counter = CharStringCount("hello word!");
+
+        Console.WriteLine("Finishing");
     }
 
-    Dictionary<char, int> CharStringCount(string stringToCount)
+    static Dictionary<char, int> CharStringCount(string stringToCount)
     {
         // declare initial dictionary
         Dictionary<char, int> counter = new Dictionary<char, int>();
@@ -41,8 +45,19 @@ class Program
         for (var i = 0; i < stringToCount.Length; i++)
         {
             // if char is character or number and not exist in the dictionary
+            bool checkLetters = (stringToCount[i] >= 65 && stringToCount[i] <= 90) ||
+                                (stringToCount[i] >= 97 && stringToCount[i] <= 122);
+            if (!counter.ContainsKey(stringToCount[i]) && checkLetters)
+            {
                 // add it and counter = 1
+                counter.Add(stringToCount[i], 0);
+            }
+            
             // if char the character exist in the dictionary 
+            if (counter.ContainsKey(stringToCount[i]) && checkLetters)
+            {
+                counter[stringToCount[i]]++;
+            }
             // add it in the dictionary
             // return the dictionary
         }
